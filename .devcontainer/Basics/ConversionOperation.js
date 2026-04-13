110a4c1e746d57b679f8e311ddcb0ed0b05088cd
nowshadhasan152092-dotcom <nosadhasan152092@gmail.com>
2026-04-13 14:31:01 +0000

    ConvesionOperation

---
+++ .devcontainer/Basics/ConversionOperation.js
@@ -0,0 +1,15 @@
+let score = "33"
+console.log(typeof score);
+console.log(typeof(score));
+
+
+
+
+let valueInNumber = Number(score)
+
+console.log(valueInNumber)        // 33
+console.log(typeof valueInNumber) // number
+
+let isloggedIn = 1
+let b = Boolean(isloggedIn)
+console.log(b);
